a2cd267b85faeaca96e955417a2b0e0643a0b329
Aleksei Terentev <aleksei.terentev@NetCracker.com>
2021-12-14 19:27:44 +0400

    Initial commit

---
+++ src/main/java/ru/gb/alekseiterentev/geek_university_java_953_crm/GeekUniversityJava953CrmApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.alekseiterentev.geek_university_java_953_crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekUniversityJava953CrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekUniversityJava953CrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gb/alekseiterentev/geek_university_java_953_crm/GeekUniversityJava953CrmApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.alekseiterentev.geek_university_java_953_crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekUniversityJava953CrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
